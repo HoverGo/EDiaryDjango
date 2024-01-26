3a1f07e98766abfc7746cc530ab92cce19a96e2d
HoverGo <kotikarbuz@gmail.com>
2024-01-26 14:28:00 +0500

    Bootstrap + header + create user and news models

--- ediary/ediary/urls.py
+++ ediary/ediary/urls.py
@@ -2,8 +2,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from main.views import *
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path("", include("main.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
