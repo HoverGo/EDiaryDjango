ecfe40d369bb9376faf4652ef10325c2431feba3
ryzhenkov_na <ryzhenkov_na@tgt72.ru>
2024-10-16 08:24:27 +0500

    config diary record db

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class DiaryRecord(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, unique=True)
+    date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(max_length=2048)
+
+    def __str__(self):
+        return f"{self.name}: {self.date}"
+    
+    class Meta:
+        verbose_name_plural = "Diary records"
